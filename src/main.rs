90650a7c993a640f829889116feab66d2f3acd7a
araya <araya@araya.dev>
2023-07-22 18:04:53 +0900

    Compile vcl written in hbs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+
+use handlebars::{to_json, Handlebars};
+use serde_json::Map;
+
 fn main() {
-    println!("Hello, world!");
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_template_file("vcl_main", "src/vcl/main.vcl.hbs")
+        .unwrap();
+
+    let mut data = Map::new();
+    data.insert("debugText".to_string(), to_json("hello world"));
+
+    let mut output_file = File::create("dist/main.vcl").unwrap();
+
+    handlebars
+        .render_to_write("vcl_main", &data, &mut output_file)
+        .unwrap();
 }
